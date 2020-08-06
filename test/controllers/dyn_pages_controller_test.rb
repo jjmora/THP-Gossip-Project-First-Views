3968b329bf1aa3ce0ddf8521456d60174a3dedb8
bakbledy93 <phim.kevin@gmail.com>
2020-08-06 10:28:27 +0200

    Delete the useless controllers

--- test/controllers/dyn_pages_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class DynPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get gossiper" do
-    get dyn_pages_gossiper_url
-    assert_response :success
-  end
-
-end
--- test/controllers/welcome_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get welcome_message" do
-    get welcome_welcome_message_url
-    assert_response :success
-  end
-
-end
